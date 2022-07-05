aefb016bba97463dcbc618d1cf88509061296fc0
vishnukai <vishnuka.me197@gmail.com>
2022-07-05 16:52:09 +0530

    views and temeplate deleted

--- tallyapp/views.py
+++ tallyapp/views.py
@@ -11,6 +11,7 @@ from dateutil import relativedelta
 from django.db.models import Q
 
 
+
 # Create your views here.
 def home(request):
     return render(request,'base.html')
@@ -21,9 +22,21 @@ def chequeprinting(request):
 def chequeregister(request):
     # bab=bank.objects.values('ledger').Count('ledger')
     # print(bab)
+    v=Vouchertype.objects.all()
+    for v in v:
+        if v.vouchertype=="Contra":
+            vid=v.id
+        elif v.vouchertype=="Payment":
+            pid=v.id
+        else:
+            pass
 
+    t=transactiontype.objects.all()
+    for t in t:
+        if t.transactiontype=="Cheque":
+            tid=t.id
 
-    b=bank.objects.filter(Q(vouchertype='1')|Q (vouchertype='2')).filter(transactiontype=1).values('ledger').annotate(total=Count('ledger'))
+    b=bank.objects.filter(Q(vouchertype=vid)|Q (vouchertype=pid)).filter(transactiontype=tid).values('ledger').annotate(total=Count('ledger'))
     print(b)
     bak=ledger.objects.all()
 
@@ -59,11 +72,7 @@ def chequeregister(request):
 
 def searchbar(request):
     group=groups.objects.get(group="Bank Account")
-    
-
     led=ledger.objects.filter(group=group.id)
-    
-
     return render(request,'searchbar.html',{'l':led})
 
 def searchledger(request):
@@ -76,12 +85,20 @@ def chequep(request,id):
     sum=0
     led=ledger.objects.get(id=id)
     uid=led.id
-    tran=transactiontype.objects.all()
-    for t in tran:
+    v=Vouchertype.objects.all()
+    for v in v:
+        if v.vouchertype=="Contra":
+            vid=v.id
+        elif v.vouchertype=="Payment":
+            pid=v.id
+        else:
+            pass
+    t=transactiontype.objects.all()
+    for t in t:
         if t.transactiontype=="Cheque":
             tid=t.id
 
-    bak=bank.objects.filter(ledger=uid).filter(Q(vouchertype='1')|Q (vouchertype='2')).filter(transactiontype=tid)
+    bak=bank.objects.filter(Q(vouchertype=vid)|Q (vouchertype=pid)).filter(transactiontype=tid)
     back=bak
     for  back in back:
         b=back.amount.amount
@@ -92,8 +109,6 @@ def chequep(request,id):
 def voucher(request,id):
     bak=bank.objects.get(id=id)
     uid=bak.amount.id
-    
-    
     if contra.objects.filter(amount=uid).exists():
         con=contra.objects.get(amount=uid)
         led=ledger.objects.all()     
@@ -107,9 +122,6 @@ def voucher(request,id):
         con=receipt.objects.get(amount=uid)
         led=ledger.objects.all()
         return render(request,'receipt.html',{'bak':bak,'con':con,'led':led})
-
-
-
 
 def updatepayment(request,id):
     if request.method=="POST":
@@ -153,9 +165,9 @@ def updatepayment(request,id):
             dateid=accot
             cond.date=dateid
             cond.amount=amountid
-            cond.ledger=ledaccount
+            cond.ledger=ledparticulars
             cond.save()
-            bak.ledger=ledaccount
+            bak.ledger=ledparticulars
             bak.amount=part
             bak.date=accot
             bak.save()
@@ -301,10 +313,10 @@ def updateconvertpayment(request,id):
         part.save()
         amountid=part
         dateid=accot
-        con=contra(vouchertype=v,ledger=ledaccount,no=no,amount=amountid,date=dateid)
+        con=contra(vouchertype=v,ledger=ledparticulars,no=no,amount=amountid,date=dateid)
         con.save()
         bak.vouchertype=v
-        bak.ledger=ledaccount
+        bak.ledger=ledparticulars
         bak.amount=part
         bak.date=accot
         bak.save()
@@ -553,25 +565,63 @@ def instrument(request,id):
 
             
 def monthlysummary(request, id):
-
     trans=Vouchertype.objects.all()
+    todays_date = datetime.date.today()
     for t in trans:
         if t.vouchertype=="Receipt":
             tid=t.id
-
-
-
     a=bank.objects.filter(ledger=id).values_list('id','instdate').filter(vouchertype=tid)
     print(a)
     b=dict(a)
-    print(b)
     
-    for keys, values in b.items():
-        print(values.month)   
+    v=Vouchertype.objects.all()
+    for v in v:
+        if v.vouchertype=="Contra":
+            vid=v.id
+        elif v.vouchertype=="Payment":
+            pid=v.id
+        else:
+            pass
+    z=transactiontype.objects.all()
+    for z in z:
+        if z.transactiontype=="Cheque":
+            zid=z.id
+    new=bank.objects.filter(ledger=id).values_list('id','instdate').filter(Q(vouchertype=vid)|Q (vouchertype=pid)).filter(transactiontype=zid)
+    newb=dict(new)
+    newamount=bank.objects.filter(ledger=id).values('instdate','amount').filter(Q(vouchertype=vid)|Q (vouchertype=pid)).filter(transactiontype=zid)
+    newtotal={}
+    for i in range(1,13):
+         newj=0
+         newtotal[i]=0
+         for course in newamount:
+            if i==(course['instdate'].month):
+                if (course['instdate'].month)>todays_date.month:
+                    e=course['amount']
+                    par=Particulars.objects.get(id=e)
+                    newj=newj+par.amount
+                    newtotal[i]=newj
+                elif (course['instdate'].day)>=todays_date.day:
+                    e=course['amount']
+                    par=Particulars.objects.get(id=e)
+                    newj=newj+par.amount
+                    newtotal[i]=newj
+                else:
+                    pass
+    newmonth={}
+    for i in range(1,13):
+        newjan=0
+        newmonth[i]=0
+        for keys, values in newb.items():
+            if i==values.month:
+                if values.month > todays_date.month:
+                    newjan=newjan+1
+                    newmonth[i]=newjan
+                elif values.day>=todays_date.day:
+                    newjan=newjan+1
+                    newmonth[i]=newjan
+                else:
+                    pass
     
-        
-        print(values.day)
-
     todays_date = datetime.date.today()
     fiscalyear.setup_fiscal_calendar(start_month=4)
     fy = FiscalYear.current()
@@ -581,161 +631,30 @@ def monthlysummary(request, id):
     #     next_month_start = d + relativedelta.relativedelta(months=m, day=1)
     #     print(next_month_start.strftime('%Y-%m-%d'))
     month={}
-    may=0
-    june=0
-    july=0
-    august=0
-    sep=0
-    oct=0
-    nov=0
-    dec=0
-    jan=0
-    feb=0
-    march=0
+ 
     print(todays_date.day)
     
-    for keys, values in b.items():    
-      
-        if 1==values.month:
-            if values.month > todays_date.month:
-                jan=jan+1
-                month[1]=jan
-            elif values.day>=todays_date.day:
-                jan=jan+1
-                month[1]=jan
-            else:
+    for i in range(1,13):   
+        jan=0
+        month[i]=0
+        for keys, values in b.items():
+            if i==values.month:
+                if values.month > todays_date.month:
+                    jan=jan+1
+                    month[i]=jan
+                elif values.day>=todays_date.day:
+                    jan=jan+1
+                    month[i]=jan
+                else:
+                    pass
 
-                pass
-
-        elif 2==values.month:
-            if values.month > todays_date.month:
-                feb=feb+1
-                month[2]=feb
-            elif values.day >= todays_date.day:
-                feb=feb+1
-                month[2]=feb
-            else:
-                pass
-            
-        elif 3==values.month:
-            if values.month > todays_date.month:
-                march=march+1
-                month[3]=march
-            elif values.day >= todays_date.day:
-                march=march+1
-                month[3]=march
-            else:
-                pass
-        elif 4==values.month:
-            if values.month > todays_date.month:
-                april=april+1
-                month[4]=april
-            elif values.day >= todays_date.day:
-                april=april+1
-                month[4]=april
-            else:
-                pass
-        elif 5==values.month:
-            if values.month > todays_date.month:
-                may=may+1
-                month[5]=may
-            elif values.day >= todays_date.day:
-                may=may+1
-                month[5]=may
-            else:
-                pass
-        elif 6==values.month:
-            if values.month > todays_date.month:
-                june=june+1
-                month[6]=june
-            elif values.day >= todays_date.day:
-                june=june+1
-                month[6]=june
-            else:
-                pass
-        elif 7==values.month:
-            if values.month > todays_date.month:
-                july=july+1
-                month[7]=july
-                print(july)
-            elif  values.day >= todays_date.day:
-                july=july+1
-                month[7]=july
-                print(july)
-            else:
-                pass
-        elif 8==values.month:
-            if values.month > todays_date.month:
-                august=august+1
-                month[8]=august
-            elif values.day >= todays_date.day:
-                august=august+1
-                month[8]=august
-            else:
-                pass
-        elif 9==values.month:
-            if values.month > todays_date.month:
-                sep=sep+1
-                month[9]=sep
-            elif values.day >= todays_date.day:
-                sep=sep+1
-                month[9]=sep
-            else:
-                pass
-        elif 10==values.month:
-            if values.month > todays_date.month:
-                oct=oct+1
-                month[10]=oct
-            elif values.day >= todays_date.day:
-                oct=oct+1
-                month[10]=oct
-            else:
-                pass
-        elif 11==values.month:
-            if values.month > todays_date.month:
-                nov=nov+1
-                month[11]=nov
-            elif values.day >= todays_date.day:
-                nov=nov+1
-                month[11]=nov
-            else:
-                pass
-        elif 12==values.month:
-            if values.month > todays_date.month:
-                dec=dec+1
-                month[12]=dec
-            elif values.day>= todays_date.day:
-                dec=dec+1
-                month[12]=dec
-            else:
-                pass
-        else:
-            pass
-    print(month)
     amount=bank.objects.filter(ledger=id).values('instdate','amount').filter(vouchertype=tid)
-    
-    
-   
-
     total={}
-    ma=0
-    j=0
-    ju=0
-    au=0
-    s=0
-    o=0
-    n=0
-    de=0
-    j=0
-    f=0
-    mar=0
-    ap=0
-      
-
-
-
-    for course in amount:
-        for i in range(1,13):
+ 
+    for i in range(1,13):
+        j=0
+        total[i]=0
+        for course in amount:
             if i==(course['instdate'].month):
                 if (course['instdate'].month)>todays_date.month:
                     e=course['amount']
@@ -750,161 +669,27 @@ def monthlysummary(request, id):
                 else:
                     pass
 
-                
-            elif i==(course['instdate'].month):
-                if (course['instdate'].month)>todays_date.month:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    f=f+par.amount
-                    total[i]=f
-                
-                elif(course['instdate'].day)>=todays_date.day:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    f=f+par.amount
-                    total[i]=f
-                else:
-                    pass
-            elif i==(course['instdate'].month):
-                if (course['instdate'].month)>todays_date.month:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    mar=mar+par.amount
-                    total[i]=mar
-                elif (course['instdate'].day)>=todays_date.day:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    mar=mar+par.amount
-                    total[i]=mar
-                else:
-                    pass
-            elif i==(course['instdate'].month):
-                if (course['instdate'].month)>todays_date.month:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    ap=ap+par.amount
-                    total[i]=ap
-                elif (course['instdate'].day)>=todays_date.day:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    ap=ap+par.amount
-                    total[i]=ap
-                else:
-                    pass
-            elif i==(course['instdate'].month):
-                if (course['instdate'].month)>todays_date.month:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    ma=ma+par.amount
-                    total[i]=ma
-                elif (course['instdate'].day)>=todays_date.day:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    ma=ma+par.amount
-                    total[i]=ma
-                else:
-                    pass
-            elif i==(course['instdate'].month):
-                if (course['instdate'].month)>todays_date.month:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    j=j+par.amount
-                    total[i]=j
-                elif (course['instdate'].day)>=todays_date.day:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    j=j+par.amount
-                    total[i]=j
-                else:
-                    pass
+    fiscalyear.setup_fiscal_calendar(start_month=4)
+    fy = FiscalYear.current()
+    
+    
+    current_fiscal_year = FiscalYear.current()
+    
+    d = datetime.datetime(current_fiscal_year.start.year, current_fiscal_year.start.month, current_fiscal_year.start.day)
+    f=datetime.datetime(current_fiscal_year.end.year, current_fiscal_year.end.month, current_fiscal_year.end.day)
+    next_month_start = d + relativedelta.relativedelta(months=0, day=1)
+    s=next_month_start.strftime("%d %b, %Y")
+    new=f.strftime("%d %b, %Y")
 
-            elif i==(course['instdate'].month):
-                if (course['instdate'].month)>todays_date.month:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    ju=ju+par.amount
-                    total[i]=ju
-                elif (course['instdate'].day)>=todays_date.day:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    ju=ju+par.amount
-                    total[i]=ju
-                else:
-                    pass
-            elif i==(course['instdate'].month):
-                if (course['instdate'].month)>todays_date.month:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    au=au+par.amount
-                    total[i]=au
-                elif (course['instdate'].day)>=todays_date.day:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    au=au+par.amount
-                    total[i]=au
-                else:
-                    pass
-            elif i==(course['instdate'].month):
-                if (course['instdate'].month)>todays_date.month:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    s=s+par.amount
-                    total[i]=s
-                elif (course['instdate'].day)>todays_date.day:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    s=s+par.amount
-                    total[i]=s
-                else:
-                    pass
-            elif i==(course['instdate'].month):
-                if (course['instdate'].month)>todays_date.month:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    o=o+par.amount
-                    total[i]=o
-                elif (course['instdate'].day)>=todays_date.day:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    o=o+par.amount
-                    total[i]=o
-                else:
-                    pass
-            elif i==(course['instdate'].month):
-                if (course['instdate'].month)>todays_date.month:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    n=n+par.amount
-                    total[i]=n
-                elif (course['instdate'].day)>=todays_date.day:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    n=n+par.amount
-                    total[i]=n
-                else:
-                    pass
-            elif i==(course['instdate'].month):
-                if (course['instdate'].month)>todays_date.month:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    de=de+par.amount
-                    total[i]=de
-                elif(course['instdate'].day)>=todays_date.day:
-                    e=course['amount']
-                    par=Particulars.objects.get(id=e)
-                    de=de+par.amount
-                    total[i]=de
-            else:
-                pass
     bak=bank.objects.filter(ledger=id)
-    print(total)
-    print(amount)
-    return render(request,'montlysummary.html',{'month':month,'total':total,'bak':bak,'id':id})
+
+    return render(request,'montlysummary.html',{'month':month,'total':total,'bak':bak,'id':id,'s':s,'new':new,'newmonth':newmonth,'newtotal':newtotal})
 
 def getjune(request,id):
     fiscalyear.setup_fiscal_calendar(start_month=6)
     fy = FiscalYear.current()
     current_fiscal_year = FiscalYear.current()
+    todays_date = datetime.date.today()
     d = datetime.datetime(current_fiscal_year.start.year, current_fiscal_year.start.month, current_fiscal_year.start.day)
     for m in range(0, 1):
         for n in range(0,1):
@@ -916,15 +701,17 @@ def getjune(request,id):
                 if t.vouchertype=="Receipt":
                     tid=t.id 
                     
-            rec=bank.objects.filter(instdate__gte=next_month_start,instdate__lte=next_month_end,ledger=id).filter(vouchertype=tid)
+            rec=bank.objects.filter(instdate__gte=todays_date,instdate__lte=next_month_end,ledger=id).filter(vouchertype=tid)
             print(rec)
     return render(request,'receiptbank.html')
 
 def getjuly(request,id):
     fiscalyear.setup_fiscal_calendar(start_month=7)
     fy = FiscalYear.current()
+
     current_fiscal_year = FiscalYear.current()
     d = datetime.datetime(current_fiscal_year.start.year, current_fiscal_year.start.month, current_fiscal_year.start.day)
+    todays_date = datetime.date.today()
     for m in range(0, 1):
         for n in range(0,1):
             next_month_start = d + relativedelta.relativedelta(months=m, day=1)
@@ -935,7 +722,7 @@ def getjuly(request,id):
                 if t.vouchertype=="Receipt":
                     tid=t.id 
                     
-            rec=bank.objects.filter(instdate__gte=next_month_start,instdate__lte=next_month_end,ledger=id).filter(vouchertype=tid)
+            rec=bank.objects.filter(instdate__gte=todays_date,instdate__lte=next_month_end,ledger=id)
             print(rec)
             
     return render(request,'receiptbank.html',{'rec':rec})
